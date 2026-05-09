78b3f9f9dd90eaf9a3212eb8cb50feb97e4f867d
Samarth M M <samarthmm7@gmail.com>
2026-05-10 01:08:03 +0530

    django: custom User model

--- Python/2_Django/config/settings/base.py
+++ Python/2_Django/config/settings/base.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+    'apps.users',
+    'apps.blog',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
